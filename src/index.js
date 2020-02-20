e001ad104ac67e94d7a4f8be974d44f9851feed8
wrublik93 <wrublik.93@mail.ru>
2020-02-20 03:54:33 +0300

    feat: implement towerSort function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix && matrix.length != 0) {
+      for(let i= 0; i<matrix.length; i++){
+          if(i % 2 != 0){
+              matrix[i].reverse();
+          }
+      }
+      return matrix.reduce(function(a, b){
+          return a.concat(b);
+      });
+  } else {
+      return [];
+  }
 }
